e1bf7401dc1f902da8bfe60d4727017d60ec3eab
Matheus Balão <matheus.balao@hotmail.com>
2021-09-29 16:13:52 -0300

    Insert Tarot cards - Minor and Major Arcana

---
+++ tarot-cards.py
@@ -0,0 +1,236 @@
+# TAROT CARDS
+# MINOR ARCANA
+minor_tarot_cards = [
+    {
+        "wards": {
+            "Knight": "The fiery part of fire. A man of activity, generosity, pride and swiftness. Cruelty, bigotry, petulance.",
+            "Queen": "The watery part of fire. A woman of adaptability, persistent energy, calm authority, powers of attraction, generous but intolerant. Obstinacy, revenge, dominance.",
+            "Prince": "The airy part of fire. A young man, swift and strong, impulsive, violent, just, noble and generous with a sense of humor. Proud, intolerant, cruel, cowardly, and prejudiced.",
+            "Princess": "The earthy part of fire. An energetic young woman, individualistic, brilliant and daring, expressive in love or anger, enthusiastic. Superficial, theatrical, shallow, cruel, unreliable, faithless.",
+            "Ace": "The Root of the Powers of Fire. Energy. Strength. Natural Force. Sexual vigor",
+            "Two": "Dominion. Fire in its highest form. Force of energy. Harmony of power and justice. Influence. Boldness. Courage. Fierceness. Restlessness. Turbulence. Obstinacy",
+            "Three": "Virtue. Established strength. Success after struggle. Pride and arrogance. Realization of hope. Conceit.",
+            "Four": "Completion. Perfection. Settlement. Rest. Subtlety. Cleverness. Knowledge brings conclusions. Unreliable outcomes from overzealous action.",
+            "Five": "Strife. Quarreling. Fighting. Competition. Cruelty. Violence. Lust. Desire. Generosity or excess spending.",
+            "Six": "Victory. Balanced energy. Love. Gain and success. Triumph after strife. Influenced: Insolence and pride.",
+            "Seven": "Honor. Struggles. Small victories. Courage to meet obstacles. Victory in small things. Quarreling",
+            "Eight": "Swiftness. Speech. Light. Electricity. Energy of high velocity. Activity. Approaching goals. Letter. Message. Boldness. Freedom. Too much force applied too suddenly",
+            "Nine": "Strength. Power. Health. Success after conflict. Tremendous force. Recovery. Victory follows fear. Change brings stability.",
+            "Ten": "Oppression. Force detached from spiritual sources. Fire in its most destructive aspect. Cruelty and malice. Selfishness. Lying. Repression. Slander. Ill will. Influenced: Self-sacrifice and generosity."
+        },
+        "cups": {
+            "Knight": "The fiery part of water. A man with commitment issues. Amiable but passive. Attracted to excitement. Unsustainable enthusiasm. Sensitive but shallow. Influenced: Sensual and idle, untruthful, prone to depression and drug abuse.",
+            "Queen": "The watery part of water. An observer, dreamy, tranquil, poetic, imaginative, kind yet passive. Impressionable to other card influences.",
+            "Prince": "The airy part of water. A young man of subtlety, secret violence, craft. An artist whose calm surface masks intense passion. Ruthless in his aims. Ambitious and obtuse.",
+            "Princess": " The earthy part of water. A young woman, infinitely gracious, sweet, voluptuous, gentle, kind, romantic and dreamy. Indolent, selfish and luxurious woman",
+            "Ace": "The Root of the Powers of Water. Fertility. Productivity. Beauty. Pleasure and happiness.",
+            "Two": "Love. Harmony of male and female sensibilities. Radiant joy. Ecstasy. Pleasure. Warm friendship. Intimacy. Carelessness. Dissipation. Waste",
+            "Three": "Abundance. Spiritual fertility. Plenty. Hospitality. Pleasure. Sensuality. Passive success. Love. Kindness. Bounty. Transient pleasure.",
+            "Four": "Luxury. Weakness. Abandonment to desire. Pleasure mixed with anxiety. Injustice. The seeds of decay in the fruits of pleasure.",
+            "Five": "Disappointment. Unexpected disturbance. Misfortune. Heartache. Unkindness from friends. Betrayal. Resentment. Sadness. Regret.",
+            "Six": ": Pleasure. Well-being. Effortless harmony. Ease. Satisfaction. Happiness. Success. Fulfillment of sexual will. Beginnings of improvements. Presumptuous. Vain. Thankless.",
+            "Seven": "Delusion. Illusory success. Drug addiction. Intoxication. Guilt. Lying. Deceit. Promises unfulfilled. Lust. Dissipation of love and friendship",
+            "Eight": "Indolence. Abandoned success. Declining interest. Temporary success. Instability. Misery. Transience which may lead away from material success.",
+            "Nine": "Happiness. Complete success. Pleasure. Physical well-being. Vanity, conceit and overindulgence.",
+            "Ten": "Contentment. Pursuit of pleasure. Desired outcome. Success. Peacemaking. Generosity. Dissipation. Overindulgence. Pity. Waste. Stagnation."
+        },
+        "swords": {
+            "Knight": "The fiery part of air. An active man, skillful and clever. Fierce and courageous, but often unreflective. A man incapable of decision, deceitful and over-bearing.",
+            "Queen": "The watery part of air. A graceful woman, intensely perceptive, a keen observer, subtle interpreter, an intense individualist. Confident and gracious. Also: Cruel, sly, deceitful and unreliable woman. Superficially attractive.",
+            "Prince": "The airy part of air. A young intellectual man, full of ideas and designs, domineering, intensely clever but unstable. Elusive. Impressionable. Influenced: Harsh, malicious, plotting, unreliable, fanatic.",
+            "Princess": "The earthy part of air. A young woman, stern and revengeful, with destructive logic, firm and aggressive, skilled in practical affairs. Influenced: Cunning, frivolous, and manipulative",
+            "Ace": "The Root of the Powers of Air. Invoked force. Power for good or evil. Conquest. Activity. Strength through trouble. Just punishment",
+            "Two": "Peace. Dual nature. Sacrifice and trouble giving birth to strength. Conflict leading to peace. Pleasure after pain. Truth and untruth. Indecision. Ambivalence.",
+            "Three": "Sorrow. Melancholy. Unhappiness. Tears. Disruption. Discord. Delay. Absence. Separation. Deceit. Faith. Honesty.",
+            "Four": "Truce. Rest from sorrow. Peace after war. Relief from anxiety. Recovery. Change after struggle. Intellectual authority. Convention.",
+            "Five": "Defeat. Loss. Malice. Spite. Weakness. Slander. Failure. Anxiety, Poverty. Dishonor. Trouble. Grief. Ties. Gossip. Interference",
+            "Six": "Science. Directed intelligence. Labor. Work. Success after challenge. Passage from difficulty. Journey by water. Influenced: Self-centeredness. Intellectual conceit.",
+            "Seven": "Futility. Unstable effort. Vacillation. Striving in vain. Incomplete success due to exhaustion. Journey by land. Untrustworthy person.",
+            "Eight": "Interference. Misdirected energy. Neglect of important matters. Lack of persistence. Unforeseen bad luck. Restriction. Great care in some areas, disorder in others",
+            "Nine": "Cruelty. Mental anguish. Despair. Hopelessness. Worry. Suffering. Loss. Illness. Malice. Burden. Oppression. Lying. Shame. Influenced: Obedience. Faithfulness. Patience. Unselfishness.",
+            "Ten": "Ruin. Faulty reasoning. Death. Failure. Disruption. Clever. Eloquent but impertinent person. Spiritually, may herald the end of delusion."
+        },
+        "disks": {
+            "Knight": "The fiery part of earth. A farmer, patient, laborious and clever. Somewhat dull and preoccupied with material things. Influenced: Avaricious, surly, petty, jealous.",
+            "Queen": "The watery part of earth. A ambitious woman, yet affectionate and kind, charming, timid, practical, quiet and domesticated. Influenced: Dull. Servile. Foolish. Capricious. Moody.",
+            "Prince": "The airy part of earth. An energetic young man. A capable manager and steadfast worker, competent, perhaps dull, somewhat skeptical of spirituality, slow to anger but implacable if aroused.",
+            "Princess": "The earthy part of earth. A young woman, beautiful and strong, pregnant with life. She is generous, kind, diligent, and benevolent. Influenced: Wasteful and at war with her essential dignity.",
+            "Ace": "The Root of the Powers of Earth. Material gain. Power. Labor. Wealth. Contentment. Materialism.",
+            "Two": "Change. Harmony. Alternating gain and loss, weakness and strength, elation and melancholy. Varying occupation. Wandering. Visit to friends. Pleasant change. Industrious, yet unreliable.",
+            "Three": "Work endeavors. Business. Commercial transaction. Constructive. Increase of material things. Growth. Commencement of projects. Influenced: Selfish, narrow, unrealistic, greedy.",
+            "Four": "Power. Law and order. Gain of money and influence. Success. Rank. Dominion. Physical skill. Influenced: Prejudice. Envy. Suspicion. Lack of originality",
+            "Five": "Worry. Intense strain. Inactivity. Financial loss. Professional setbacks. Monetary anxiety. Poverty. Influenced: Labor. Real estate. Business acumen.",
+            "Six": "Success. Material gain. Power. Influence. Philanthropy. Transitory situation. Influenced: Insolence. Conceit with wealth. Excessive spending.",
+            "Seven": "Failure. Unfinished work. Unprofitable speculation. Unmet goals. Hopes deceived. Disappointment. Little gain from much effort. Influenced: Delayed growth. Honorable undertakings.",
+            "Eight": "Prudence. Intelligence in material affairs. Agriculture. Building. Skill. Cunning. Industriousness. Influenced: “Penny wise and pound foolish.” Avarice. Meanness. Failure to see the big picture.",
+            "Nine": "Gain. Good fortune. Inheritance. Improved wealth. Influenced: Envy, loss, waste.",
+            "Ten": "Wealth. Prosperity. Creativity. Old age. Influenced: Laziness. Indifference. Dullness of mind."
+        }
+    }
+]
+
+# MAJOR CARDS
+major_tarot_cards = [
+    {
+        "code": 0,
+        "name": "The Fool",
+        "meaning": "In spiritual matters, represents ideas and thoughts, which endeavor to transcend earth. In material matters, reveals folly, eccentricity, even mania. It represents a sudden, unexpected impulse.\nThis is the moment of divine consciousness",
+        "hebrew_letter": "Aleph",
+        "planet": "Emptiness"
+    },
+    {
+        "code": 1,
+        "name": "The Magus",
+        "meaning": "Skill. Wisdom. Adroitness. Elasticity. Craft. Cunning. Deceit. Theft. Sometimes esoteric wisdom or power. Messages. Business transactions. Learning or intelligence interfering with the matter in hand.",
+        "hebrew_letter": "beth",
+        "planet": "Mercury"
+    },
+    {
+        "code": 2,
+        "name": "The Priestess",
+        "meaning": "Pure, exalted and gracious influence enters the matter, bringing change, alternation, increase and decrease, fluctuation. Exuberance should be tempered and careful balance maintained.",
+        "hebrew_letter": "Gimel",
+        "planet": "The Moon"
+    },
+    {
+        "code": 3,
+        "name": "The Empress",
+        "meaning": "Love. Beauty. Happiness. Pleasure. Success. Fruitfulness. Good fortune. Graciousness. Elegance. Gentleness. Influenced: Dissipation. Promiscuity. Idleness. Sensuality.",
+        "hebrew_letter": "Daleth",
+        "planet": "Venus"
+    },
+    {
+        "code": 4,
+        "name": "The Emperor",
+        "meaning": "War. Conquest. Victory. Strife. Power. Stability. Originality. Government. Energy. Ambition. Influenced: Arrogance. Megalomania. Rashness.",
+        "hebrew_letter": "Tzaddi",
+        "planet": "Aries"
+    },
+    {
+        "code": 5,
+        "name": "The Hierophant",
+        "meaning": "Divine wisdom. Inspiration. Stubborn strength. Toil. Endurance. Persistence. Teaching. Help from superiors. Patience. Organization. Peace. Goodness of heart.",
+        "hebrew_letter": "Vau",
+        "planet": "Taurus"
+    },
+    {
+        "code": 6,
+        "name": "The Lovers",
+        "meaning": "Inspiration. Intuition. Intelligence. Innocence. Attraction. Beauty. Love. Influenced: Self-contradiction. Instability. Indecision. Superficiality. Infatuation. Union of opposites.",
+        "hebrew_letter": "Zain",
+        "planet": "Gemini"
+    },
+    {
+        "code": 7,
+        "name": "The Chariot",
+        "meaning": "Triumph. Victory. Hope. Obedience. Faithfulness. Health. Success, though sometimes not enduring. Influenced: Abrupt departure from traditional ideas.",
+        "hebrew_letter": "Cheth",
+        "planet": "Cancer"
+    },
+    {
+        "code": 8,
+        "name": "Adjustment or Justice",
+        "meaning": "Justice. Balance. Adjustment. Suspension of action pending decision. May refer to lawsuits, trials, marriages, contracts, etc",
+        "hebrew_letter": "Teth",
+        "planet": "Libra"
+    },
+    {
+        "code": 9,
+        "name": "The Hermit",
+        "meaning": "Illumination from within. Divine inspiration. Wisdom. Circumspection. Retirement.",
+        "hebrew_letter": "Yod",
+        "planet": "Virgo"
+    },
+    {
+        "code": 10,
+        "name": "Fortune",
+        "meaning": "Change of fortune, generally good. Destiny",
+        "hebrew_letter": "Kaph",
+        "planet": "Jupiter"
+    },
+    {
+        "code": 11,
+        "name": "Lust or Strength",
+        "meaning": "Courage. Strength. Energy. Use of magical power. Control of the life force. Great love affair. Joy in Desire.",
+        "hebrew_letter": "Teth",
+        "planet": "Leo"
+    },
+    {
+        "code": 12,
+        "name": "The Hanged Man or Dying God",
+        "meaning": "Redemption through sacrifice. New perspectives. Punishment. Loss. Defeat. Failure. Suffering",
+        "hebrew_letter": "Mem",
+        "planet": "Water"
+    },
+    {
+        "code": 13,
+        "name": "Death",
+        "meaning": "Transformation. Change, voluntary or involuntary, perhaps sudden and unexpected. Illusory death. Release through destruction. ",
+        "hebrew_letter": "Nun",
+        "planet": "Scorpio"
+    },
+    {
+        "code": 14,
+        "name": "Art or Temperance",
+        "meaning": "Combination of forces. Realization. Action based on accurate calculation. Economy. Management. Success after elaborate maneuvers. Escape. The path of the True Will.",
+        "hebrew_letter": "",
+        "planet": ""
+    },
+    {
+        "code": 15,
+        "name": "The Devil",
+        "meaning": "Blind impulse. Irresistibly strong. Unscrupulous. Ambition. Temptation. Obsession. Secret plan. Hard work. Endurance. Discontent. Materialism. Fate.",
+        "hebrew_letter": "Ain",
+        "planet": "Capricornus"
+    },
+    {
+        "code": 16,
+        "name": "The Tower or The House of God",
+        "meaning": "Quarrel. Combat. Danger. Ruin. Destruction of plans. Ambition. Courage. Sudden death. Escape from prison and all it implies.",
+        "hebrew_letter": "Peh",
+        "planet": "Mars"
+    },
+    {
+        "code": 17,
+        "name": "The Star",
+        "meaning": "Hope. Unexpected help. Clarity of vision. Spiritual insight. Influenced: Dreaminess. Disappointment",
+        "hebrew_letter": "Daleth",
+        "planet": "Aquarius"
+    },
+    {
+        "code": 18,
+        "name": "The Moon",
+        "meaning": "Illusion. Deception. Bewilderment. Falsehood. Voluntary change. Influenced: Hysteria. Madness.Hidden darkness which must be passed before light can be reborn.",
+        "hebrew_letter": "Koph",
+        "planet": "Pisces"
+    },
+    {
+        "code": 19,
+        "name": "The Sun",
+        "meaning": "Glory. Gain. Riches. Triumph. Pleasure. Truth. Shamelessness. Manifestation. Recovery. Arrogance. Vanity",
+        "hebrew_letter": "Resh",
+        "planet": "Sun"
+    },
+    {
+        "code": 20,
+        "name": "The Aeon, or The Angel or the Last Judgement",
+        "meaning": "Closure. Resolution. Definitive action. Unlimited possibility.",
+        "hebrew_letter": "Shin",
+        "planet": "Fire"
+    },
+    {
+        "code": 21,
+        "name": "The Universe",
+        "meaning": "Essential questions. Synthesis. Delay. Completion. Opposition. Inertia. Perseverance. Patience. Crystallized thinking. ",
+        "hebrew_letter": "Tau",
+        "planet": "Saturn"
+    }
+]
+
+# INVOCATION OF THE GOLDEN DAWN
+initial_invocation = """I invoke Thee, IAO, that thou wilt send HRU, the great
+Angel that is set over the operations of this secret Wisdom, to
+lay his hand invisibly on these consecrated cards of art, that
+thereby we may obtain true knowledge of hidden things to
+the glory of thine ineffable Name. Amen"
+"""
